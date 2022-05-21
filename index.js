d1a472befaeee84d7fa8d76609da7b79eedd6b53
NickeaBennett <nickeabennett@gmail.com>
2022-05-21 11:22:30 -0400

    Assignment

--- index.js
+++ index.js
@@ -8,6 +8,58 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ const createEmployeeRecord = ([firstName, familyName, title, payPerHour]) => {
+    return {
+       firstName,
+       familyName,
+       title,
+       payPerHour,
+       timeInEvents : [],
+       timeOutEvents : []
+   }
+}
+
+const createEmployeeRecords = (employeeRecords) => {
+   return employeeRecords.map(createEmployeeRecord);
+   
+}
+
+const createTimeInEvent = (record, timeStamp) => {
+
+   const timeObj = {
+       type: "TimeIn",
+       date: timeStamp.split(" ")[0],
+       hour: parseInt(timeStamp.slice(-4), 10)
+   }
+   record.timeInEvents.push(timeObj)
+   return record
+}
+
+const createTimeOutEvent = (record, timeStamp) => {
+
+   const timeObj = {
+       type: "TimeOut",
+       date: timeStamp.split(" ")[0],
+       hour: parseInt(timeStamp.slice(-4), 10)
+   }
+   record.timeOutEvents.push(timeObj)
+   return record
+}
+
+const hoursWorkedOnDate = (record, date) => {
+   const timeIn = record.timeInEvents.find(e => {
+       return e.date === date
+   }).hour
+   const timeOut = record.timeOutEvents.find(e => {
+       return e.date === date
+   }).hour
+   debugger
+   return (timeOut - timeIn) / 100
+}
+
+const wagesEarnedOnDate = (record, date) => {
+   return hoursWorkedOnDate(record, date) * record.payPerHour
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -21,3 +73,10 @@ const allWagesFor = function () {
     return payable
 }
 
+const calculatePayroll = (arr) => {
+    const totalPay = arr.reduce((acc, record) => {
+        const totalPayForEmployee = allWagesFor(record)
+        return acc + totalPayForEmployee
+    }, 0)
+    return totalPay
+  }
